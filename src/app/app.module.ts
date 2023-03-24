5bd431c90e1691ab9e3d36df108943c9fabc61ca
Makaruos Mekhail <119800650+makaruos-mekhail@users.noreply.github.com>
2023-03-24 23:39:37 +0200

    add components all_category slidbar_allcategory home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AllCategoryComponent } from './components/all-category/all-category.component';
+import { SlidbarAllcategoryComponent } from './components/slidbar-allcategory/slidbar-allcategory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AllCategoryComponent,
+    SlidbarAllcategoryComponent
   ],
   imports: [
     BrowserModule
