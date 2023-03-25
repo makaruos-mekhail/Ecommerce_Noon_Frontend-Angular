36e4b69e94807850c13b165648dba9204775b8cc
Makaruos Mekhail <119800650+makaruos-mekhail@users.noreply.github.com>
2023-03-25 16:14:29 +0200

    add component Main & NotFound & app-routing &add-medule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { AllCategoryComponent } from './components/all-category/all-category.component';
+import { CartComponent } from './components/cart/cart.component';
+import { DetailsComponent } from './components/details/details.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { WishListComponent } from './wish-list/wish-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { SlidbarAllcategoryComponent } from './components/slidbar-allcategory/slidbar-allcategory.component';
+import { WishListComponent } from './components/wish-list/wish-list.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NotfoundComponent,
     HeaderComponent,
     FooterComponent,
-    WishListComponent
+    SlidbarAllcategoryComponent,
+    WishListComponent,
+    HomeComponent,
+    DetailsComponent,
+    AllCategoryComponent,
+    CartComponent,
+    MainPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
