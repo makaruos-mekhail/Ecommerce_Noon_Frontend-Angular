8c7edfb4ac7c00e9e53a6ba5c96330091c7b5f6d
Makaruos Mekhail <119800650+makaruos-mekhail@users.noreply.github.com>
2023-04-01 05:58:49 +0200

    Add Validation login & sgin in

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,7 +4,7 @@ import { ICategory } from "src/app/Models/icategory";
 import { CategoyService } from "src/app/Services/categoy.service";
 import { ProductService } from "src/app/Services/product.service";
 import { Location } from '@angular/common';
-import { TranslateService } from "@ngx-translate/core";
+import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-header",
@@ -13,38 +13,47 @@ import { TranslateService } from "@ngx-translate/core";
 })
 export class HeaderComponent implements  OnInit{
 
-
   categoriesList: ICategory[] = [];
 
 
-  constructor(
-    private categoryservice: CategoyService,
-    private router: Router,
-    private translate: TranslateService
-  )
+  userFormGroup: FormGroup;
+  userloginFormGroup: FormGroup;
+
+  constructor(private categoryservice: CategoyService,private productservice: ProductService,private router :Router, private location :Location,private formBulider : FormBuilder)
   {
        //Get Dropdown category
        this.categoryservice.getAllCategories().subscribe((data) => {
        console.log(data);
        this.categoriesList = data;
        });
+
+
+
+       this.userFormGroup = this.formBulider.group({
+        // create Account
+        ufirstName : ['',[Validators.required,Validators.pattern('[a-zA-Z0-9]{4,10}')]],
+        ulastName : ['',[Validators.required,Validators.pattern('[a-zA-Z0-9]{4,10}')]],
+        Uemail :['',[Validators.required,Validators.pattern('[a-zA-Z0-9]{4,15}@(gmail|yahoo)(.com)')]],
+        Upassword :['',[Validators.required,Validators.pattern('[a-zA-Z]{4,10}[0-9]{2}@')]],
+
+
+        // login in
+        Emaillogin :['',[Validators.required,Validators.pattern('[a-zA-Z0-9]{4,15}@(gmail|yahoo)(.com)')]],
+        Passwordlogin :['',[Validators.required,Validators.pattern('[a-zA-Z]{4,10}[0-9]{2}@')]],
+      });
+
+
+
+      this.userloginFormGroup = this.formBulider.group({
+
+      });
+
   }
 
 
   ngOnInit(): void {
 
-    
-    // handleLanguage(event: any) {
-    //   this.translate.use(event.target.value);
-    //   console.log(this.translate.currentLang);
-    // }
   }
-//translata
-  handleLanguage(event: any) {
-    this.translate.use(event.target.value);
-  }
-
- 
 
   searchpro(str :any){
     this.router.navigate(['AllCategory',str]);
@@ -52,5 +61,28 @@ export class HeaderComponent implements  OnInit{
   }
 
 
-}
 
+
+    //convert firstname to property
+    get Fname(){
+      return this.userFormGroup.get('ufirstName');
+    }
+    get Lname(){
+      return this.userFormGroup.get('ulastName');
+    }
+    get EmailU(){
+      return this.userFormGroup.get('Uemail');
+    }
+    get passwordU(){
+      return this.userFormGroup.get('Upassword');
+    }
+
+
+
+    get EmailLogin(){
+      return this.userFormGroup.get('Emaillogin');
+    }
+    get PassLogin(){
+      return this.userFormGroup.get('Passwordlogin');
+    }
+}
