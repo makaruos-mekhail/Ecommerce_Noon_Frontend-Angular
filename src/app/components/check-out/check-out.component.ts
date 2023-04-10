8c9b46455e3435583adca81833b6c1167645f273
Mostafa Hassan <mostafa157716@gmail.com>
2023-04-10 14:23:30 +0200

    Asking order

--- src/app/components/check-out/check-out.component.ts
+++ src/app/components/check-out/check-out.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { render } from 'creditcardpayments/creditCardPayments';
+import { CookieService } from 'ngx-cookie-service';
+import { IOrder } from 'src/app/Models/iorder';
+import { OrderService } from 'src/app/Services/order.service';
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
@@ -7,38 +10,51 @@ import { render } from 'creditcardpayments/creditCardPayments';
 })
 export class CheckOutComponent {
   lang = localStorage.getItem('lang');
-  constructor() {
+  paid! : boolean;
+  email = this.cookieService.get("useremail");
+  totalPrice = JSON.parse(this.cookieService.get('cart')).reduce((acc: any, item: any) => acc + item.price, 0);
+  cart = JSON.parse(this.cookieService.get('cart'));
+
+  constructor(private orderService: OrderService, private cookieService: CookieService) { 
     render({
       id: "#myPaypalButtons",
       currency: "EGP",
-      value: "100.00",
+      value: this.totalPrice.toString(),
+      
       onApprove: (details) => {
-        alert("Transaction Successfull");
-        console.log("Transaction Successfull");
+        let button=document.getElementById("order_btn") as HTMLButtonElement;
+        button.disabled = false;
+        this.paid = true;     
       },
-    })
-    }
-    
-    
-//    removeDisabled(){
+    }) 
+  }
 
-// input?.setAttribute("checked","true")
-    
 
-//   btn?.removeAttribute("disabled")};
- 
+//   removeDisabled
 removeDisabled(){
-  var checkbox=document.getElementById("checkbox") as HTMLInputElement
-   var button=document.getElementById("order_btn") as HTMLButtonElement
-
+ let checkbox=document.getElementById("checkbox") as HTMLInputElement;
+ let button=document.getElementById("order_btn") as HTMLButtonElement;
+// checkbox.checked || 
   if (checkbox.checked) {
     button.disabled = false;
-  } else {
+  }
+   else {
     button.disabled = true;
-  
+  }
 }
-  
-}
-   
-}
-  
+
+
+order : IOrder | undefined;
+  sendOrder(){
+    console.log("send order");
+    this.order = {
+      username: this.email,
+      orderItemsDTO: this.cart,
+      totalPrice: this.totalPrice,
+      paymentMethod: "ayhagaaaa"
+    }
+    this.orderService.sendOrder(this.order).subscribe((data) => {
+      console.log(data);
+    })
+  }
+}  
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   APIURLProduct: 'https://localhost:7006/api',
-  ProductUrl:'https://localhost:7006/api//Product'
+  ProductUrl:'https://localhost:7006/api//Product',
+  GoogleMaps: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDqy2ik5FNnIlyU4PqA-MYwFdyw7WVz6xM'
 };
